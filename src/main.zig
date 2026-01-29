b5e637301e8fd4f19d644a8f5659f11958436b95
Ryoga-exe <git@ryoga.dev>
2026-01-29 23:15:15 +0900

    feat: デカい文字

--- src/main.zig
+++ src/main.zig
@@ -10,36 +10,48 @@ pub export fn main() callconv(.c) noreturn {
     const p: [*]u8 = @ptrFromInt(start);
     @memset(p[0..len], 0);
 
-    uart.putString("booting ChiffonOS\n\n");
+    uart.putString("booting ChiffonOS...\n\n");
 
-    const pair = mb.readFbPair();
-    if (pair.fb0 == 0) {
-        uart.putString("mailbox fb0=0\n");
-        while (true) {}
-    }
+    uart.putString(
+        \\   _____ _     _  __  __             ____   _____ 
+        \\  / ____| |   (_)/ _|/ _|           / __ \ / ____|
+        \\ | |    | |__  _| |_| |_ ___  _ __ | |  | | (___  
+        \\ | |    | '_ \| |  _|  _/ _ \| '_ \| |  | |\___ \ 
+        \\ | |____| | | | | | | || (_) | | | | |__| |____) |
+        \\  \_____|_| |_|_|_| |_| \___/|_| |_|\____/|_____/ 
+        \\
+    );
 
-    uart.putString("[INFO] Frame buffer is OK, initialize graphics\n");
+    while (true) {}
 
-    var gfx = gfxm.Gfx.init(pair.fb0, pair.fb1);
-
-    uart.putString("[INFO] Graphics initialized\n");
-
-    var frame: u32 = 0;
-    while (true) : (frame += 1) {
-        gfx.begin();
-        gfx.clear(0x0000_0000);
-
-        const rw: u32 = 120;
-        const rh: u32 = 90;
-        const x0: u32 = (frame * 3) % (gfxm.width - rw);
-        const y0: u32 = (frame * 2) % (gfxm.height - rh);
-
-        gfx.fillRect(x0, y0, rw, rh, 0x00FF_0000);
-        gfx.fillRect((x0 + 160) % (gfxm.width - rw), (y0 + 90) % (gfxm.height - rh), rw, rh, 0x0000_FF00);
-        gfx.fillRect((x0 + 320) % (gfxm.width - rw), (y0 + 180) % (gfxm.height - rh), rw, rh, 0x0000_00FF);
-
-        gfx.endAndPresent();
-    }
+    // const pair = mb.readFbPair();
+    // if (pair.fb0 == 0) {
+    //     uart.putString("mailbox fb0=0\n");
+    //     while (true) {}
+    // }
+    //
+    // uart.putString("[INFO] Frame buffer is OK, initialize graphics\n");
+    //
+    // var gfx = gfxm.Gfx.init(pair.fb0, pair.fb1);
+    //
+    // uart.putString("[INFO] Graphics initialized\n");
+    //
+    // var frame: u32 = 0;
+    // while (true) : (frame += 1) {
+    //     gfx.begin();
+    //     gfx.clear(0x0000_0000);
+    //
+    //     const rw: u32 = 120;
+    //     const rh: u32 = 90;
+    //     const x0: u32 = (frame * 3) % (gfxm.width - rw);
+    //     const y0: u32 = (frame * 2) % (gfxm.height - rh);
+    //
+    //     gfx.fillRect(x0, y0, rw, rh, 0x00FF_0000);
+    //     gfx.fillRect((x0 + 160) % (gfxm.width - rw), (y0 + 90) % (gfxm.height - rh), rw, rh, 0x0000_FF00);
+    //     gfx.fillRect((x0 + 320) % (gfxm.width - rw), (y0 + 180) % (gfxm.height - rh), rw, rh, 0x0000_00FF);
+    //
+    //     gfx.endAndPresent();
+    // }
 }
 
 pub export fn _start() linksection(".text.init") callconv(.naked) noreturn {
